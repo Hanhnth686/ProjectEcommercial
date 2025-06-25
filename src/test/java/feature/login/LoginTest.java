39b491e19728a04bfa3055307b102d5700057ea5
Hanhnth686 <hanhnth686@gmail.com>
2025-06-25 16:15:19 +0700

    First commit

--- src/test/java/feature/login/LoginTest.java
+++ src/test/java/feature/login/LoginTest.java
@@ -3,65 +3,101 @@ package feature.login;
 import action.LoginPage;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import unltils.Common;
+import unltils.ExcelUntils;
+
+import java.util.List;
+import java.util.Map;
 
 public class LoginTest extends Common {
     LoginPage loginPage = new LoginPage(driver);
+    String excelFilePath = "dataLogin.xlsx";
 
     @BeforeMethod
     public void setupPage() {
         loginPage = new LoginPage(driver);
     }
+
     public void inputData(String user, String pass) {
         loginPage.enterUsername(user);
         loginPage.enterPassword(pass);
     }
+
     public void clickLogin() {
         loginPage.clickLogin();
     }
-    @Test
-    public void inputValidCredentials() {
-        inputData("standard_user", "secret_sauce");
+
+    @DataProvider(name = "loginData")
+    public Object[][] loginData() {
+        List<Map<String, String>> excelData = ExcelUntils.readExcelData(excelFilePath, "Sheet1");
+        Object[][] data = new Object[excelData.size()][1];
+        for (int i = 0; i < excelData.size(); i++) {
+            data[i][0] = excelData.get(i);
+        }
+        return data;
+    }
+
+    @Test(dataProvider = "loginData")
+    public void inputValidCredentials(Map<String, String> rowData) {
+        String username = rowData.getOrDefault("Username", "");
+        String password = rowData.getOrDefault("Password", "");
+
+        inputData(username, password);
         clickLogin();
+
         Assert.assertTrue(loginPage.isLogoDisplayed(), "Logo is not displayed!");
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html", "Login failed!");;
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html", "Login failed!");
+        ;
         Assert.assertTrue(loginPage.getInventoryItemCount() > 0, "Login failed!");
     }
-    @Test
-    public void inputEmptyUsername() {
-        inputData("", "secret_sauce");
-        clickLogin();
-        Assert.assertTrue(loginPage.displayErrorButton(), "Website don't display error button!");
-        Assert.assertEquals(loginPage.getErrorMessage(),"Epic sadface: Username is required!", "Error message mismatch!");
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/", "Website still login without Username!");
-    }
-    @Test
-    public void inputInvalidUsername() {
-        inputData("invalid_user", "secret_sauce2");
-        clickLogin();
-        Assert.assertTrue(loginPage.displayErrorButton(), "Website don't display error button!");
-        Assert.assertEquals(loginPage.getErrorMessage(),"Epic sadface: Username and password do not match any user in this service", "Error message mismatch!");
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/", "Website still login with invalid Username!");
 
+    @Test
+    public void inputEmptyUsername(Map<String, String> rowData) {
+        String username = rowData.getOrDefault("Username", "");
+        String password = rowData.getOrDefault("Password", "");
+
+        inputData(username, password);
+        clickLogin();
+        Assert.assertTrue(loginPage.displayErrorButton());
+        Assert.assertEquals(loginPage.getErrorMessage(), "Epic sadface: Username is required");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/");
     }
 
     @Test
-    public void inputEmptyPassword() {
-        inputData("standard_user", "");
+    public void inputInvalidUsername(Map<String, String> rowData) {
+        String username = rowData.getOrDefault("Username", "");
+        String password = rowData.getOrDefault("Password", "");
+
+        inputData(username, password);
         clickLogin();
-        Assert.assertTrue(loginPage.displayErrorButton(), "Website don't display error button!");
-        Assert.assertEquals(loginPage.getErrorMessage(),"Epic sadface: Password is required", "Error message mismatch!");
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/", "Website still login without Password!");
+        Assert.assertTrue(loginPage.displayErrorButton());
+        Assert.assertEquals(loginPage.getErrorMessage(), "Epic sadface: Username and password do not match any user in this service");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/");
     }
+
     @Test
-    public void inputInvalidPassword() {
-        inputData("standard_user", "invalid_password");
-        clickLogin();
-        Assert.assertTrue(loginPage.displayErrorButton(), "Website don't display error button!");
-        Assert.assertEquals(loginPage.getErrorMessage(),"Epic sadface: Username and password do not match any user in this service", "Error message mismatch!");
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/", "Website still login with invalid Password!");
+    public void inputEmptyPassword(Map<String, String> rowData) {
+        String username = rowData.getOrDefault("Username", "");
+        String password = rowData.getOrDefault("Password", "");
 
+        inputData(username, password);
+        clickLogin();
+        Assert.assertTrue(loginPage.displayErrorButton());
+        Assert.assertEquals(loginPage.getErrorMessage(), "Epic sadface: Password is required");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/");
     }
 
+    @Test
+    public void inputInvalidPassword(Map<String, String> rowData) {
+        String username = rowData.getOrDefault("Username", "");
+        String password = rowData.getOrDefault("Password", "");
+
+        inputData(username, password);
+        clickLogin();
+        Assert.assertTrue(loginPage.displayErrorButton());
+        Assert.assertEquals(loginPage.getErrorMessage(), "Epic sadface: Username and password do not match any user in this service");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/");
+    }
 }
--- src/test/java/ui/InventoryUI.java
+++ src/test/java/ui/InventoryUI.java
@@ -4,4 +4,5 @@ import org.openqa.selenium.By;
 
 public class InventoryUI { // Lớp InventoryUI đại diện cho trang Inventory
     public static final By INVENTORY_ITEM = By.className("inventory_item");
+    public static final By REMOVE_BUTTON = By.xpath("//button[@id='remove-sauce-labs-backpack']");
 }
